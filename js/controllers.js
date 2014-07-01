32f8f3320a2888261c76a2176e92b24a7e733bb4
Jeff Worford <jworford@gmail.com>
2014-07-01 08:32:02 -0500

    Create service functions.

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,10 @@
 /* Controllers */
 
 angular.module('webhooksio.controllers', [])
-  .controller('WebhookCtrl', ['$scope', '$location', '$http', 'consumerService', function($scope, $location, $http, consumerService) {
+  .controller('WebhookCtrl', ['$scope', '$location', '$http', '$templateCache', 'consumerService', function($scope, $location, $http, $templateCache, consumerService) {
 
   $scope.changePage = function($page) {
+    $templateCache.removeAll();
     $scope.currentview = $page;
   }
 
@@ -40,41 +41,23 @@ angular.module('webhooksio.controllers', [])
   $scope.application_id = $scope.params.application_id;
   $scope.application_version_id = $scope.params.application_version_id;
   $scope.consumer_id = $scope.params.consumer_id;
-  $scope.consumer_name = $scope.params.consumer_name;
   $scope.api_token = $scope.params.token;
 
   // Default authorization
   $http.defaults.headers.common.Authorization = 'Bearer ' + $scope.api_token;
 
-  //Check to see if the customer is setup
-  consumerService.getConsumer($scope.urlbase, $scope.apiversion, $scope.account_id, $scope.application_id, $scope.consumer_id).success(function(data) {
-    //Consumer is Setup
-    console.log(data);
-  }).error(function(data) {
-    if(data.status_code == 404) {
-
-      //Consumer is not setup, lets provision them.
-      var postdata =  $.param({
-          consumer_id: $scope.consumer_id,
-          name : $scope.consumer_name
-      });
-
-      consumerService.provisionConsumer($scope.urlbase, $scope.apiversion, $scope.account_id, $scope.application_id, postdata).success(function(data) {
-        console.log(data);
-      }).error(function(data) {
-         $scope.message = data.message || "Request failed";
-        $scope.messagedetails = data.message_detail;
-        $scope.showError = true;
-      });
-
-      
-      console.log('provision consumer here');
-    } else {
-      $scope.message = data.message || "Request failed";
-      $scope.messagedetails = data.message_detail;
-      $scope.showError = true;
-    }
-   });
+  
   	
+
+  }])
+  .controller('OutputCtrl', ['$scope', '$location', '$http', 'consumerService', function($scope, $location, $http, consumerService) {
+
+  // Default authorization
+  $http.defaults.headers.common.Authorization = 'Bearer ' + $scope.api_token;
+
+  console.log("loaded");
+
+  
+    
 
   }]);
--- js/services.js
+++ js/services.js
@@ -20,58 +20,45 @@ angular.module('webhooksio.services', [])
 
                });
           },
-          provisionConsumer: function($urlbase, $apiversion, $account_id, $application_id, $postparams) {
+          createOutput: function($urlbase, $apiversion, $account_id, $application_id, $consumer_id, $postparams) {
                $http.defaults.useXDomain = true;
                $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
                delete $http.defaults.headers.common['X-Requested-With'];
-               return $http.post($urlbase + '/' + $apiversion + '/accounts/' + $account_id + '/applications/' + $application_id + '/consumers', $postparams).success(function(result) {
+               return $http.post($urlbase + '/' + $apiversion + '/accounts/' + $account_id + '/applications/' + $application_id + '/consumers/' + $consumer_id + '/outputs', postparams).success(function(result) {
                     return result.data;
                }).error(function(result, status) {
                     return result;
-
                });
-          }/*,
-          getBucket: function($bucket_id) {
+          },
+          getOutputs:  function($urlbase, $apiversion, $account_id, $application_id, $consumer_id) {
                $http.defaults.useXDomain = true;
                delete $http.defaults.headers.common['X-Requested-With'];
-               return $http.get($localStorage.urlbase + '/' + $localStorage.apiversion + '/accounts/' + $localStorage.account_id + '/buckets/' + $bucket_id).success(function(result) {
+               return $http.get($urlbase + '/' + $apiversion + '/accounts/' + $account_id + '/applications/' + $application_id + '/consumers/' + $consumer_id + '/outputs').success(function(result) {
                     return result.data;
                }).error(function(result, status) {
                     return result;
 
                });
           },
-          deleteBucket: function($bucket_id) {
-               $http.defaults.useXDomain = true;
-               delete $http.defaults.headers.common['X-Requested-With'];
-               return $http.delete($localStorage.urlbase + '/' + $localStorage.apiversion + '/accounts/' + $localStorage.account_id + '/buckets/' + $bucket_id).success(function(result) {
-                    return result.data;
-               }).error(function(result, status) {
-                    return result;
-
-               });
-          },
-          createBucket: function($postparams) {
+          updateOutput:  function($urlbase, $apiversion, $account_id, $application_id, $consumer_id, $output_id, $postparams) {
                $http.defaults.useXDomain = true;
                $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
                delete $http.defaults.headers.common['X-Requested-With'];
-               return $http.post($localStorage.urlbase + '/' + $localStorage.apiversion + '/accounts/' + $localStorage.account_id + '/buckets', $postparams).success(function(result) {
+               return $http.put($urlbase + '/' + $apiversion + '/accounts/' + $account_id + '/applications/' + $application_id + '/consumers/' + $consumer_id + '/outputs/' + $output_id, postparams).success(function(result) {
                     return result.data;
                }).error(function(result, status) {
                     return result;
-
                });
           },
-          updateBucket: function($bucket_id, $postparams) {
+          deleteOutput: function($urlbase, $apiversion, $account_id, $application_id, $consumer_id) {
                $http.defaults.useXDomain = true;
-               $http.defaults.headers.put["Content-Type"] = "application/x-www-form-urlencoded";
                delete $http.defaults.headers.common['X-Requested-With'];
-               return $http.put($localStorage.urlbase + '/' + $localStorage.apiversion + '/accounts/' + $localStorage.account_id + '/buckets/' + $bucket_id, $postparams).success(function(result) {
+               return $http.delete($urlbase + '/' + $apiversion + '/accounts/' + $account_id + '/applications/' + $application_id + '/consumers/' + $consumer_id + '/outputs/' + $output_id).success(function(result) {
                     return result.data;
                }).error(function(result, status) {
                     return result;
 
                });
-          }*/
+          }
      }
 })
